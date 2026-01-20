6f54e87a4532871f24ab37961108735787cc9cb0
hainesnoids <127229@lrstudents.org>
2026-01-20 11:18:30 -0500

    start working on file formats

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const electron = require('electron');
 const express = require('express');
 const expressWs = require('@wll8/express-ws');
 const path = require('node:path');
-// const fs = require('node:fs');
+const fs = require('node:fs');
 
 // const package = require('./package.json');
 const electronInit = require('./electron-init.js');
@@ -13,6 +13,7 @@ const PORT = 45549;
 
 app.use(express.static(path.join(__dirname, 'webroot')));
 app.use('/dashboard', express.static(path.join(__dirname, 'dashboard')));
+app.use('/usrdata', express.static(path.join(__dirname, 'usrdata')));
 
 app.listen(PORT, () => {console.log('Started JumPres Server')});
 electronInit.create(electron);
